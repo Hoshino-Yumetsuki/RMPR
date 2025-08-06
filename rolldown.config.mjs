e01efa70d73a03b1ffbf30e3763e1e60bc86ac69
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-08-07 00:31:35 +0800

    refactor: 更新打包器

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,9 +1,12 @@
+import resolve from '@rollup/plugin-node-resolve'
+
 export default {
   input: 'src/main.ts',
   output: {
     file: 'dist/main.js',
-    minify:true
+    minify: true
   },
+  external: [/node_modules/],
   platform: 'node',
-  minify:true
+  plugins: [resolve()]
 }
